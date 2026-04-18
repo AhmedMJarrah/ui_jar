7949b10970c812463081f4057c34adc50b8430a2
Ahmed Jarrah <146374586+AhmedMJarrah@users.noreply.github.com>
2026-04-18 13:34:02 +0300

    Change initial sidebar state to expanded

--- files/app.py
+++ files/app.py
@@ -9,7 +9,7 @@ st.set_page_config(
     page_title="مراجع التشريعات",
     page_icon="⚖️",
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded",
 )
 
 # ─── CSS ───────────────────────────────────────────────────────────────────────
